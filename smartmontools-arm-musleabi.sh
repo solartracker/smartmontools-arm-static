2acf672b5bccf4c3b31043e402922def367a93e2
Richard Elwell <229418400@users.noreply.github.com>
2026-01-12 15:31:55 -0500

    compile with arm-linux-musleabi

---
+++ smartmontools-arm-musleabi.sh
@@ -0,0 +1,566 @@
+#!/bin/sh
+################################################################################
+# smartmontools-arm-musleabi.sh
+#
+# Copyright (C) 2025 Richard Elwell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
+PATH_CMD="$(readlink -f -- "$0")"
+SCRIPT_DIR="$(dirname -- "$(readlink -f -- "$0")")"
+PARENT_DIR="$(dirname -- "$(dirname -- "$(readlink -f -- "$0")")")"
+CACHED_DIR="${PARENT_DIR}/solartracker-sources"
+set -e
+set -x
+
+################################################################################
+# Helpers
+
+# If autoconf/configure fails due to missing libraries or undefined symbols, you
+# immediately see all undefined references without having to manually search config.log
+handle_configure_error() {
+    local rc=$1
+
+    #grep -R --include="config.log" --color=always "undefined reference" .
+    #find . -name "config.log" -exec grep -H "undefined reference" {} \;
+    find . -name "config.log" -exec grep -H -E "undefined reference|can't load library|unrecognized command-line option" {} \;
+
+    # Force failure if rc is zero, since error was detected
+    [ "${rc}" -eq 0 ] && return 1
+
+    return ${rc}
+}
+
+################################################################################
+# Package management
+
+# new files:       rw-r--r-- (644)
+# new directories: rwxr-xr-x (755)
+umask 022
+
+# Checksum verification for downloaded file
+verify_hash() {
+    [ -n "$1" ] || return 1
+
+    local file="$1"
+    local expected="$2"
+    local option="$3"
+    local actual=""
+
+    if [ ! -f "${file}" ]; then
+        echo "ERROR: File not found: ${file}"
+        return 1
+    fi
+
+    if [ -z "${option}" ]; then
+        # hash the compressed binary file. this method is best when downloading
+        # compressed binary files.
+        actual="$(sha256sum "${file}" | awk '{print $1}')"
+    elif [ "${option}" == "tar_extract" ]; then
+        # hash the data, file names, directory names. this method is best when
+        # archiving Github repos.
+        actual="$(tar -xJOf "${file}" | sha256sum | awk '{print $1}')"
+    elif [ "${option}" == "xz_extract" ]; then
+        # hash the data, file names, directory names, timestamps, permissions, and
+        # tar internal structures. this method is not as "future-proof" for archiving
+        # Github repos because it is possible that the tar internal structures
+        # could change over time as the tar implementations evolve.
+        actual="$(xz -dc "${file}" | sha256sum | awk '{print $1}')"
+    else
+        return 1
+    fi
+
+    if [ "${actual}" != "${expected}" ]; then
+        echo "ERROR: SHA256 mismatch for ${file}"
+        echo "Expected: ${expected}"
+        echo "Actual:   ${actual}"
+        return 1
+    fi
+
+    echo "SHA256 OK: ${file}"
+    return 0
+}
+
+retry() {
+    local max=$1
+    shift
+    local i=1
+    while :; do
+        if ! "$@"; then
+            if [ "${i}" -ge "${max}" ]; then
+                return 1
+            fi
+            i=$((i + 1))
+            sleep 10
+        else
+            return 0
+        fi
+    done
+}
+
+wget_clean() {
+    [ -n "$1" ]          || return 1
+    [ -n "$2" ]          || return 1
+    [ -n "$3" ]          || return 1
+
+    local temp_path="$1"
+    local source_url="$2"
+    local target_path="$3"
+
+    rm -f "${temp_path}"
+    if ! wget -O "${temp_path}" --tries=9 --retry-connrefused --waitretry=5 "${source_url}"; then
+        rm -f "${temp_path}"
+        return 1
+    else
+        if ! mv -f "${temp_path}" "${target_path}"; then
+            rm -f "${temp_path}" "${target_path}"
+            return 1
+        fi
+    fi
+
+    return 0
+}
+
+download()
+( # BEGIN sub-shell
+    [ -n "$1" ]            || return 1
+    [ -n "$2" ]            || return 1
+    [ -n "$3" ]            || return 1
+    [ -n "${CACHED_DIR}" ] || return 1
+
+    local source_url="$1"
+    local source="$2"
+    local target_dir="$3"
+    local cached_path="${CACHED_DIR}/${source}"
+    local target_path="${target_dir}/${source}"
+    local temp_path=""
+
+    if [ ! -f "${cached_path}" ]; then
+        mkdir -p "${CACHED_DIR}"
+        if [ ! -f "${target_path}" ]; then
+            cleanup() { rm -f "${cached_path}" "${temp_path}"; }
+            trap 'cleanup; exit 130' INT
+            trap 'cleanup; exit 143' TERM
+            trap 'cleanup' EXIT
+            temp_path=$(mktemp "${cached_path}.XXXXXX")
+            if ! retry 100 wget_clean "${temp_path}" "${source_url}" "${cached_path}"; then
+                return 1
+            fi
+            trap - EXIT INT TERM
+        else
+            cleanup() { rm -f "${cached_path}"; }
+            trap 'cleanup; exit 130' INT
+            trap 'cleanup; exit 143' TERM
+            trap 'cleanup' EXIT
+            if ! mv -f "${target_path}" "${cached_path}"; then
+                return 1
+            fi
+            trap - EXIT INT TERM
+        fi
+    fi
+
+    if [ ! -f "${target_path}" ]; then
+        if [ -f "${cached_path}" ]; then
+            ln -sfn "${cached_path}" "${target_path}"
+        fi
+    fi
+
+    return 0
+) # END sub-shell
+
+clone_github()
+( # BEGIN sub-shell
+    [ -n "$1" ]            || return 1
+    [ -n "$2" ]            || return 1
+    [ -n "$3" ]            || return 1
+    [ -n "$4" ]            || return 1
+    [ -n "$5" ]            || return 1
+    [ -n "${CACHED_DIR}" ] || return 1
+
+    local source_url="$1"
+    local source_version="$2"
+    local source_subdir="$3"
+    local source="$4"
+    local target_dir="$5"
+    local cached_path="${CACHED_DIR}/${source}"
+    local target_path="${target_dir}/${source}"
+    local temp_dir=""
+    local timestamp=""
+
+    if [ ! -f "${cached_path}" ]; then
+        umask 022
+        mkdir -p "${CACHED_DIR}"
+        if [ ! -f "${target_path}" ]; then
+            cleanup() { rm -rf "${cached_path}" "${temp_dir}"; }
+            trap 'cleanup; exit 130' INT
+            trap 'cleanup; exit 143' TERM
+            trap 'cleanup' EXIT
+            temp_dir=$(mktemp -d "${target_dir}/temp.XXXXXX")
+            mkdir -p "${temp_dir}"
+            if ! retry 100 git clone "${source_url}" "${temp_dir}/${source_subdir}"; then
+                return 1
+            fi
+            cd "${temp_dir}/${source_subdir}"
+            if ! retry 100 git checkout ${source_version}; then
+                return 1
+            fi
+            if ! retry 100 git submodule update --init --recursive; then
+                return 1
+            fi
+            timestamp="$(git log -1 --format='@%ct')"
+            rm -rf .git
+            cd ../..
+            #chmod -R g-w,o-w "${temp_dir}/${source_subdir}"
+            tar --numeric-owner --owner=0 --group=0 --sort=name --mtime="${timestamp}" -cv -C "${temp_dir}" "${source_subdir}" | xz -zc -7e >"${cached_path}"
+            touch -d "${timestamp}" "${cached_path}"
+            rm -rf "${temp_dir}"
+            trap - EXIT INT TERM
+        else
+            cleanup() { rm -f "${cached_path}"; }
+            trap 'cleanup; exit 130' INT
+            trap 'cleanup; exit 143' TERM
+            trap 'cleanup' EXIT
+            if ! mv -f "${target_path}" "${cached_path}"; then
+                return 1
+            fi
+            trap - EXIT INT TERM
+        fi
+    fi
+
+    if [ ! -f "${target_path}" ]; then
+        if [ -f "${cached_path}" ]; then
+            ln -sfn "${cached_path}" "${target_path}"
+        fi
+    fi
+
+    return 0
+) # END sub-shell
+
+download_archive() {
+    [ "$#" -eq 3 ] || [ "$#" -eq 5 ] || return 1
+
+    local source_url="$1"
+    local source="$2"
+    local target_dir="$3"
+    local source_version="$4"
+    local source_subdir="$5"
+
+    if [ -z "${source_version}" ]; then
+        download "${source_url}" "${source}" "${target_dir}"
+    else
+        clone_github "${source_url}" "${source_version}" "${source_subdir}" "${source}" "${target_dir}"
+    fi
+}
+
+apply_patch() {
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+
+    local patch_path="$1"
+    local target_dir="$2"
+
+    if [ -f "${patch_path}" ]; then
+        echo "Applying patch: ${patch_path}"
+        if patch --dry-run --silent -p1 -d "${target_dir}/" -i "${patch_path}"; then
+            if ! patch -p1 -d "${target_dir}/" -i "${patch_path}"; then
+                echo "The patch failed."
+                return 1
+            fi
+        else
+            echo "The patch was not applied. Failed dry run."
+            return 1
+        fi
+    else
+        echo "Patch not found: ${patch_path}"
+        return 1
+    fi
+
+    return 0
+}
+
+apply_patch_folder() {
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+
+    local patch_dir="$1"
+    local target_dir="$2"
+    local patch_file=""
+    local rc=0
+
+    if [ -d "${patch_dir}" ]; then
+        for patch_file in ${patch_dir}/*.patch; do
+            if [ -f "${patch_file}" ]; then
+                if ! apply_patch "${patch_file}" "${target_dir}"; then
+                    rc=1
+                fi
+            fi
+        done
+    fi
+
+    return ${rc}
+}
+
+rm_safe() {
+    [ -n "$1" ] || return 1
+    local target_dir="$1"
+
+    # Prevent absolute paths
+    case "${target_dir}" in
+        /*)
+            echo "Refusing to remove absolute path: ${target_dir}"
+            return 1
+            ;;
+    esac
+
+    # Prevent current/parent directories
+    case "${target_dir}" in
+        "."|".."|*/..|*/.)
+            echo "Refusing to remove . or .. or paths containing ..: ${target_dir}"
+            return 1
+            ;;
+    esac
+
+    # Finally, remove safely
+    rm -rf -- "${target_dir}"
+
+    return 0
+}
+
+apply_patches() {
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+
+    local patch_dir="$1"
+    local target_dir="$2"
+
+    if ! apply_patch_folder "${patch_dir}" "${target_dir}"; then
+        #rm_safe "${target_dir}"
+        return 1
+    fi
+
+    return 0
+}
+
+extract_package() {
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+
+    local source_path="$1"
+    local target_dir="$2"
+
+    case "${source_path}" in
+        *.tar.gz|*.tgz)
+            tar xzvf "${source_path}" -C "${target_dir}"
+            ;;
+        *.tar.bz2|*.tbz)
+            tar xjvf "${source_path}" -C "${target_dir}"
+            ;;
+        *.tar.xz|*.txz)
+            tar xJvf "${source_path}" -C "${target_dir}"
+            ;;
+        *.tar.lz|*.tlz)
+            tar xlvf "${source_path}" -C "${target_dir}"
+            ;;
+        *.tar.zst)
+            tar xvf "${source_path}" -C "${target_dir}"
+            ;;
+        *.tar)
+            tar xvf "${source_path}" -C "${target_dir}"
+            ;;
+        *)
+            echo "Unsupported archive type: ${source_path}" >&2
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+unpack_archive()
+( # BEGIN sub-shell
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+
+    local source_path="$1"
+    local target_dir="$2"
+    local dir_tmp=""
+
+    if [ ! -d "${target_dir}" ]; then
+        dir_tmp=$(mktemp -d "${target_dir}.XXXXXX")
+        cleanup() { rm -rf "${dir_tmp}"; }
+        trap 'cleanup; exit 130' INT
+        trap 'cleanup; exit 143' TERM
+        trap 'cleanup' EXIT
+        mkdir -p "${dir_tmp}"
+        if extract_package "${source_path}" "${dir_tmp}"; then
+            # try to rename single sub-directory
+            if ! mv -f "${dir_tmp}"/* "${target_dir}"/; then
+                # otherwise, move multiple files and sub-directories
+                mkdir -p "${target_dir}"
+                mv -f "${dir_tmp}"/* "${target_dir}"/
+            fi
+        fi
+    fi
+
+    return 0
+) # END sub-shell
+
+is_version_git() {
+    case "$1" in
+        *+git*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+update_patch_library() {
+    [ -n "$1" ]            || return 1
+    [ -n "$2" ]            || return 1
+    [ -n "$3" ]            || return 1
+    [ -n "$4" ]            || return 1
+    [ -n "${PARENT_DIR}" ] || return 1
+    [ -n "${SCRIPT_DIR}" ] || return 1
+
+    local git_commit="$1"
+    local patches_dir="$2"
+    local pkg_name="$3"
+    local pkg_subdir="$4"
+    local entware_packages_dir="${PARENT_DIR}/entware-packages"
+
+    if [ ! -d "${entware_packages_dir}" ]; then
+        cd "${PARENT_DIR}"
+        git clone https://github.com/Entware/entware-packages
+    fi
+
+    cd "${entware_packages_dir}"
+    git fetch origin
+    git reset --hard "${git_commit}"
+    [ -d "${patches_dir}" ] || return 1
+    mkdir -p "${SCRIPT_DIR}/patches/${pkg_name}/${pkg_subdir}/entware"
+    cp -pf "${patches_dir}"/* "${SCRIPT_DIR}/patches/${pkg_name}/${pkg_subdir}/entware/"
+    cd ..
+
+    return 0
+}
+
+check_static() {
+    local rc=0
+    for bin in "$@"; do
+        echo "Checking ${bin}"
+        file "${bin}" || true
+        if readelf -d "${bin}" 2>/dev/null | grep NEEDED; then
+            rc=1
+        fi || true
+        ldd "${bin}" 2>&1 || true
+    done
+
+    if [ ${rc} -eq 1 ]; then
+        echo "*** NOT STATICALLY LINKED ***"
+        echo "*** NOT STATICALLY LINKED ***"
+        echo "*** NOT STATICALLY LINKED ***"
+    fi
+
+    return ${rc}
+}
+
+finalize_build() {
+    echo ""
+    echo "Stripping symbols and sections from files..."
+    strip -v "$@"
+
+    # Exit here, if the programs are not statically linked.
+    # If any binaries are not static, check_static() returns 1
+    # set -e will cause the shell to exit here, so renaming won't happen below.
+    echo ""
+    echo "Checking statically linked programs..."
+    check_static "$@"
+
+    # Append ".static" to the program names
+    echo ""
+    echo "Renaming programs with .static suffix..."
+    for bin in "$@"; do
+        mv -f "${bin}" "${bin}.static"
+    done
+
+    return 0
+}
+
+################################################################################
+# Install the build environment
+
+
+
+################################################################################
+# General
+
+PKG_ROOT=smartmontools
+
+CROSSBUILD_DIR="${PARENT_DIR}/cross-arm-linux-musleabi-build"
+export PREFIX="${CROSSBUILD_DIR}"
+export PATH="${CROSSBUILD_DIR}/bin:${PATH}"
+export TARGET=arm-linux-musleabi
+export HOST=${TARGET}
+
+STAGEDIR="${CROSSBUILD_DIR}"
+mkdir -p "${STAGEDIR}"
+SRC_ROOT="${CROSSBUILD_DIR}/src/${PKG_ROOT}"
+mkdir -p "${SRC_ROOT}"
+
+MAKE="make -j$(grep -c ^processor /proc/cpuinfo)" # parallelism
+#MAKE="make -j1"                                  # one job at a time
+
+export PKG_CONFIG="pkg-config"
+export PKG_CONFIG_LIBDIR="${STAGEDIR}/lib/pkgconfig"
+unset PKG_CONFIG_PATH
+
+################################################################################
+# smartmontools-7.5
+
+PKG_NAME=smartmontools
+PKG_VERSION=7.5
+PKG_SOURCE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_URL="https://github.com/smartmontools/smartmontools/releases/download/RELEASE_7_5/${PKG_SOURCE}"
+PKG_SOURCE_SUBDIR="${PKG_NAME}-${PKG_VERSION}"
+PKG_HASH="690b83ca331378da9ea0d9d61008c4b22dde391387b9bbad7f29387f2595f76e"
+
+mkdir -p "${SRC_ROOT}/${PKG_NAME}" && cd "${SRC_ROOT}/${PKG_NAME}"
+
+if [ ! -f "$PKG_SOURCE_SUBDIR/__package_installed" ]; then
+    rm -rf "$PKG_SOURCE_SUBDIR"
+    download "$PKG_SOURCE_URL" "$PKG_SOURCE" "."
+    verify_hash "$PKG_SOURCE" "$PKG_HASH"
+    unpack_archive "$PKG_SOURCE" "$PKG_SOURCE_SUBDIR"
+    cd "$PKG_SOURCE_SUBDIR"
+
+    ./configure \
+         LDFLAGS="-static" \
+         --prefix="${PREFIX}" \
+         --host="${HOST}" \
+    || handle_configure_error $?
+
+    $MAKE
+    make install
+
+    # strip and verify there are no dependencies for static build
+    finalize_build \
+        "$CROSSBUILD_DIR/sbin/smartctl" \
+        "$CROSSBUILD_DIR/sbin/smartd"
+
+    touch __package_installed
+fi
+
